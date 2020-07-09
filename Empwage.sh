7980650e170eadd8a397510154699c2b7040b9d2
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-09 00:38:21 -0700

    first commit

--- Empwage.sh
+++ Empwage.sh
@@ -3,11 +3,17 @@ echo "Welcome to employee wage computation program"
 
 wagePerHours=20
 workingHours=8
-isPresent=$(( RANDOM % 2 ))
+partTimeHours=4
+isPresent=$(( RANDOM % 3 ))
 if [ $isPresent -eq 1 ]
 then
-	echo "Employee is Present"
+	echo "Employee is Present full time"
 	dailyWage=$(( $wagePerHours * $workingHours ))
+	echo $dailyWage
+elif [ $isPresent -eq 2 ]
+then
+	echo "Employee is Present partime"
+	dailyWage=$(( $wagePerHours * $partTimeHours ))
 	echo $dailyWage
 elif [ $isPresent -eq 0 ]
 then
