5e46a46b29cd845fd7ced247e8a979b0492d69d5
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-09 02:36:58 -0700

    Added:Condition till total working days or hours reached a month

--- Empwage.sh
+++ Empwage.sh
@@ -10,20 +10,24 @@ fullTime=1
 partTime=2
 #variables
 totalWage=0
-
-for (( counter=0; counter<$workingDaysPerMonth; counter++ ))
+totalWorkingHrs=0
+totalWorkingDays=0
+while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
 	isPresent=$(( RANDOM % 3  ))
+	((totalWorkingDays++))
 	case $isPresent in
 		$fullTime)
 		echo "Employee is Present full time"
 		dailyWage=$(( $wagePerHours * $workingHours ))
 		totalWage=$(( $totalWage + $dailyWage ))
+		totalWorkingHrs=$(( $totalWorkingHrs + $workingHours ))
 		;;
 		$partTime)
 		echo "employee is  Present partime"
 		dailyWage=$(( $wagePerHours * $partTimeHours ))
 		totalWage=$(( $totalWage + $dailyWage ))
+		totalWorkingHrs=$(( $totalWorkingHrs + $workingHours ))
 		;;
 		$absent)
 		echo "Employee is Absent"
