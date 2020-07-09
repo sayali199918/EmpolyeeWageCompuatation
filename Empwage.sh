098369824a8279c5c34416d85cd65dcf0d40b95c
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-09 00:22:53 -0700

    first commit

--- Empwage.sh
+++ Empwage.sh
@@ -1,11 +1,17 @@
-##!/bin/bash -x
+#!/bin/bash -x
 echo "Welcome to employee wage computation program"
 
+wagePerHours=20
+workingHours=8
 isPresent=$(( RANDOM % 2 ))
 if [ $isPresent -eq 1 ]
 then
 	echo "Employee is Present"
+	dailyWage=$(( $wagePerHours * $workingHours ))
+	echo $dailyWage
 elif [ $isPresent -eq 0 ]
 then
 	echo "Employee is Absent"
+	dailyWage=0
+	echo $dailyWage
 fi
