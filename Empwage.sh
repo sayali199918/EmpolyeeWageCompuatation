c05724054aefcd1b4c70326a04eb6f184d600077
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-09 03:04:31 -0700

    Refactor:refactor the code to write a function for work hrs

--- Empwage.sh
+++ Empwage.sh
@@ -2,8 +2,8 @@
 echo "Welcome to employee wage computation program"
 
 wagePerHours=20
-workingHours=8
-partTimeHours=4
+#workingHours=8
+#partTimeHours=4
 workingDaysPerMonth=20
 absent=0
 fullTime=1
@@ -12,29 +12,28 @@ partTime=2
 totalWage=0
 totalWorkingHrs=0
 totalWorkingDays=0
+function getWorkHours () {
+	local checkEmp=$1
+	case $checkEmp in
+		$fullTime)
+			empHrs=8
+			;;
+		$partTime)
+			empHrs=4
+			;;
+		$absent)
+			empHrs=0
+			;;
+	esac
+echo $empHrs
+}
 while [[ $totalWorkingHrs -lt 100 && $totalWorkingDays -lt 20 ]]
 do
 	isPresent=$(( RANDOM % 3  ))
 	((totalWorkingDays++))
-	case $isPresent in
-		$fullTime)
-		echo "Employee is Present full time"
-		dailyWage=$(( $wagePerHours * $workingHours ))
-		totalWage=$(( $totalWage + $dailyWage ))
-		totalWorkingHrs=$(( $totalWorkingHrs + $workingHours ))
-		;;
-		$partTime)
-		echo "employee is  Present partime"
-		dailyWage=$(( $wagePerHours * $partTimeHours ))
-		totalWage=$(( $totalWage + $dailyWage ))
-		totalWorkingHrs=$(( $totalWorkingHrs + $workingHours ))
-		;;
-		$absent)
-		echo "Employee is Absent"
-		dailyWage=0
-		totalWage=$(( $totalWage + $dailyWage ))
-		;;
-	esac
+	workingHrs=$( getWorkHours $isPresent )
+	dailyWage=$(( $wagePerHours * $workingHrs ))
+	totalWage=$(( $totalWage + $dailyWage ))
+	totalWorkingHrs=$(( $totalWorkingHrs + $workingHrs ))
 done
 echo $totalWage
-
