fae8f4209a10780f322fbdddfe9cbe3171488b0a
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-09 01:44:27 -0700

    first commit

--- Empwage.sh
+++ Empwage.sh
@@ -4,24 +4,33 @@ echo "Welcome to employee wage computation program"
 wagePerHours=20
 workingHours=8
 partTimeHours=4
+workingDaysPerMonth=20
 absent=0
 fullTime=1
 partTime=2
-isPresent=$(( RANDOM % 3 ))
-case $isPresent in
-	$fullTime)
-	echo "Employee is Present full time"
-	dailyWage=$(( $wagePerHours * $workingHours ))
-	echo $dailyWage
-	;;
-	$partTime)
-	echo "employee is  Present partime"
-	dailyWage=$(( $wagePerHours * $partTimeHours ))
-	echo $dailyWage
-	;;
-	$absent)
-	echo "Employee is Absent"
-	dailyWage=0
-	echo $dailyWage
-	;;
-esac
+#variables
+totalWage=0
+
+for (( counter=0; counter<$workingDaysPerMonth; counter++ ))
+do
+	isPresent=$(( RANDOM % 3  ))
+	case $isPresent in
+		$fullTime)
+		echo "Employee is Present full time"
+		dailyWage=$(( $wagePerHours * $workingHours ))
+		totalWage=$(( $totalWage + $dailyWage ))
+		;;
+		$partTime)
+		echo "employee is  Present partime"
+		dailyWage=$(( $wagePerHours * $partTimeHours ))
+		totalWage=$(( $totalWage + $dailyWage ))
+		;;
+		$absent)
+		echo "Employee is Absent"
+		dailyWage=0
+		totalWage=$(( $totalWage + $dailyWage ))
+		;;
+	esac
+done
+echo $totalWage
+
